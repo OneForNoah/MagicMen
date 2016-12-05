d56e3d4249590e0d45b5dfcc3cf1edcf618af38c
noahreyes95 <95noahreyes@gmail.com>
2016-12-05 11:18:10 -0800

    .

--- newDeck.php
+++ newDeck.php
@@ -14,13 +14,13 @@
 		}
 
     echo 'test0';
-    $db = new PDO('sqlite:./database/user.db');
+    $db = new PDO('sqlite:./database/users.db');
     echo 'test1';
 	    // Set errormode to exceptions
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $un = $_POST['usern'];
-    $username = $db->query("SELECT id_user FROM users WHERE usern = '$un'");
-    $db.close();
+    $userID = $db->query("SELECT id_user FROM users WHERE username = '$un'");
+    $db = null;
 
     echo 'test2';
     $db = new PDO('sqlite:./database/mtgcard.db');
@@ -28,10 +28,12 @@
     $deckID = rand();
     $stmt = $db->prepare('INSERT INTO DeckInfo (deckID, playerID, deckName, deckSize) VALUES (:deckID, :playerID, :deckName, :deckSize);');
     $stmt->bindParam(':deckID', $deckID);
-    $stmt->bindParam(':playerID', $username);
+    $stmt->bindParam(':playerID', $userID);
     $stmt->bindParam(':deckName', $_POST['deckTitle']);
     $stmt->bindParam(':deckSize', 60);
-
+    $sdeckID = $deckID;
+    $
+    $db = null;
 	/*  //safely insert values into passengers table
 		//order matters (look at your schema) -- fname, mname, lname, ssn
     $user = $db->query('SELECT max(id_user) FROM users')
