d3bc564b5ddd6ed9496c95193bfdf693d5a64f8d
Trevor <tnunn@pugetsound.edu>
2016-12-05 01:08:51 -0800

    remove user db

--- registration/register.php
+++ registration/register.php
@@ -20,23 +20,43 @@
 	    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
+	    $sql = 'SELECT * FROM users ORDER BY id_user';
+		foreach ($db->query($sql) as $row) {
+			print $row['id_user'] . "\t";
+			print $row['name'] . "\t";
+			print $row['username'] . "\t";
+			print $row['password'] . "\n";
+		}
+		$db->query('TRUNCATE TABLE Users.users');
+		 $sql = 'SELECT * FROM users ORDER BY id_user';
+		foreach ($db->query($sql) as $row) {
+			print $row['id_user'] . "\t";
+			print $row['name'] . "\t";
+			print $row['username'] . "\t";
+			print $row['password'] . "\n";
+		}
 	    //safely insert values into passengers table
 		//order matters (look at your schema) -- fname, mname, lname, ssn
-		$stmt = $db->prepare("INSERT INTO users (id_user, name, username, password) VALUES (:id, :name, :usern, :pw)");
+		$stmt = $db->prepare('INSERT INTO users (id_user, name, username, password) VALUES (:id, :name, :usern, :pw);');
 		$stmt->bindParam(':id', $id);
 		$stmt->bindParam(':name', $name);
 		$stmt->bindParam(':usern', $usern);
 		$stmt->bindParam(':pw', $pw);
 
-		$idnum = $db->query("SELECT max(id_user) FROM users");
-		$id = echo($idnum);
+		$query = $db->query('SELECT max(id_user) AS maxval FROM users');
+		$idrow = $query->fetch(PDO::FETCH_ASSOC);
+		$max = 1;
+		echo($max);
+		$max++;
+		echo($max);
+		$id=$max ;
 		$name = $_POST['name'];
 		$usern = $_POST['usern'];
 		$pw = $_POST['pw'];
 		//echo($stmt);
 		//$db->exec($stmt);
 	    $stmt->execute();
-
+	    echo($stmt);
 	        //disconnect from database
 	    $db = null;
 	}
@@ -46,5 +66,5 @@
 	}
 
 	//redirect user to another page now
-	header("Location: index.html");
+	//header("Location: ../index.html");
 ?>
