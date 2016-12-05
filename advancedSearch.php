a8d4874744b508dce551ecff8a80b9b70c222362
Andy <avanheuit@pugetsound.edu>
2016-12-05 03:23:29 -0800

    maybe functional search I dunno it's three thirty in the morning and I'm displeased with my life choices

--- advancedSearch.php
+++ advancedSearch.php
@@ -8,21 +8,22 @@
 
 	    //safely insert values into passengers table
 		if(empty($_POST['power']) && empty($_POST['tough'])) {
-			$resultC = $db->query("SELECT FROM creatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%'");
-			$resultN = $db->query("SELECT FROM nonCreatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%'");
-			//COMBINE INTO ONE OUTPUT
+			$resultC = $db->query("SELECT * FROM creatures, nonCreatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%'");
+			//$resultN = $db->query("SELECT FROM nonCreatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%'");
+
 			//RETURN OUTPUT
+			echo $resultC;
 		} else if(empty($_POST['power'])) {
-			$resultC = $db->query("SELECT FROM creatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%' AND power=$_POST[power]");
+			$resultC = $db->query("SELECT * FROM creatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%' AND power=$_POST[power]");
 			//RETURN OUTPUT
 		} else if(empty($_POST['tough'])) {
-			$resultC = $db->query("SELECT FROM creatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%' AND tough=$_POST[tough]");
+			$resultC = $db->query("SELECT * FROM creatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%' AND tough=$_POST[tough]");
 			//RETURN OUTPUT
 		} else {
-			$resultC = $db->query("SELECT FROM creatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%' AND power=$_POST[power] AND tough=$_POST[tough]");
+			$resultC = $db->query("SELECT * FROM creatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%' AND power=$_POST[power] AND tough=$_POST[tough]");
 			//RETURN OUTPUT
 		}
-		
+
 	    //disconnect from database
 	    $db = null;
 	}
