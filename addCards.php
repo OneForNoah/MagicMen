21daf0ec103af6047328a4559059c4f367e590f1
Noah Reyes <95noahreyes@gmail.com>
2016-12-07 00:18:27 -0800

    can now add multiple copies of the same card at once

--- addCards.php
+++ addCards.php
@@ -1,9 +1,9 @@
- <?php
- try
- {
+<?php
+try
+{
   $db = new PDO('sqlite:./database/mtgcard.db');
 
-        // Set errormode to exceptions
+  // Set errormode to exceptions
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   $deck_id = $_POST['deckID'];
@@ -14,6 +14,8 @@
 
   foreach($cards as $target)
   {
+    $copies = substr($target, 0[, 1]);
+    $target = substr($target, 2[, strlen($target)]);
     $stmt = $db->prepare('INSERT INTO Decklists (deckID, cardID, numOf) VALUES (:deckID, :cardID, :numOf);');
     $stmt->bindParam(':deckID', $deckID);
     $stmt->bindParam(':cardID', $cardID);
@@ -26,16 +28,17 @@
     echo $cardID;
     //$num = $db->query("SELECT numOf FROM Decklists WHERE cardID='$cardID' AND deckID='$deckID';");
     $numOf = 1; //$num->fetchColumn(0);
-
-    $stmt->execute();
+    for($i = 0; $i<$copies; $i++ ) {
+      $stmt->execute();
+    }
   }
   $db = null;
 
-  }
-  catch(PDOException $e)
-  {
-    die('Exception : '.$e->getMessage());
-  }
+}
+catch(PDOException $e)
+{
+  die('Exception : '.$e->getMessage());
+}
 
-  header("Location: ./deckEditor.php");
+header("Location: ./deckEditor.php");
 ?>
