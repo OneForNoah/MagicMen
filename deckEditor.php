63f203ff60262bfc6fc37245b94b88d986c316d5
Trevor <tnunn@pugetsound.edu>
2016-12-06 20:12:47 -0800

    editing decks

--- deckEditor.php
+++ deckEditor.php
@@ -36,52 +36,16 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
       try
       {
         $deckID = $_POST['deck_id'];
-        echo '<FORM METHOD=POST>';
+        echo '<FORM METHOD=POST ACTION="addCards.php">';
         echo '<input type="hidden" name="deckID" value="'.$deckID.'">';
         echo '<textarea name = "cardList" placeholder="Enter card names here"rows="10" cols="100"></textarea>';
         echo '<P><INPUT TYPE=SUBMIT> <INPUT TYPE=RESET>';
         echo '</FORM>';
-
-        $db = new PDO('sqlite:./database/mtgcard.db');
-
-        // Set errormode to exceptions
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $un = $_POST['deckID'];
-        $id = $db->query("SELECT * FROM Decklists WHERE deckID = '$un';");
-        $deck_id = $id->fetchColumn(0);
-        if(empty($deck_id)) {
-          die('Exception : need real deck ID');
-        }
-        $cards = explode("\n", $_POST['cardList']);
-
-        foreach($cards as $target)
-        {
-          $stmt = $db->prepare('INSERT INTO Decklists (deckID, cardID, numOf) VALUES (:deckID, :cardID, :numOf);');
-          $stmt->bindParam(':deckID', $deckID);
-          $stmt->bindParam(':cardID', $playerID);
-          $stmt->bindParam(':numOf', $numOf);
-
-
-          $resultC = $db->query("SELECT * FROM nonCreatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%' ORDER BY cardName");
-
-          $resultN = $db->query("SELECT * FROM creatures WHERE cardName LIKE '%$_POST[name]%' AND color LIKE '%$_POST[color]%' AND cardType LIKE '%$_POST[type]%' AND cardText LIKE '%$_POST[ruletext]%' ORDER BY cardName");
-
-
-          $deckID=$id;
-          $cardID = $db->query("SELECT cardID FROM nonCreatures NATURAL JOIN creatures WHERE cardName='$target'");
-          $numOf = $db->query("SELECT numOf FROM Decklists WHERE cardName='$target'");
-          if($numOf<4) {//if there are already 4 copies of this card then do not put it into the deck
-            $stmt->execute();
-          }
-        }
-        $db = null;
-
       }
       catch(PDOException $e)
       {
       die('Exception : '.$e->getMessage());
-    }
+      }
     ?>
    <!--input name="deckID" placeholder="deckID" size="100">-->
  </div>
